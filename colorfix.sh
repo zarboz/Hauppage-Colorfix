da16b2bb9c226124f78e62f0e51a660f6692dab5
Zarboz <zarboz@gmail.com>
2012-07-28 23:24:09 -0600

    adfa

--- colorfix.sh
+++ colorfix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -v
-sleep 3s
+sleep 2s
 v4l2-ctl --set-ctrl brightness=0x80 --set-ctrl contrast=0x40 --set-ctrl hue=0xf --set-ctrl saturation=0x40 --set-ctrl sharpness=0x80 --set-ctrl=video_bitrate=13500000
 exit
