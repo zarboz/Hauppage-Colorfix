140eea7bec60017701014ae4e45001670737c581
zarboz <zarboz@gmail.com>
2012-07-28 23:19:13 -0600

    Update colorfix.sh

--- colorfix.sh
+++ colorfix.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -v
 sleep 3s
-/usr/bin/v4l2-ctl --verbose -d /dev/video0 --set-ctrl brightness=0x80 --set-ctr$
+v4l2-ctl --set-ctrl brightness=0x80 --set-ctrl contrast=0x40 --set-ctrl hue=0xf --set-ctrl saturation=0x40 --set-ctrl sharpness=0x80 --set-ctrl=video_bitrate=13500000
 exit
